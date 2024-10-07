1db138a8966482d892f08b6f2634a11f285d92c0
Harry Porter <h.porter23@icloud.com>
2024-10-07 20:55:27 +1100

    feat: adds generation script and variants

---
+++ scripts/generate.py
@@ -0,0 +1,24 @@
+import json
+import pathlib
+
+import jinja2
+
+
+def generate_jsons():
+    configurations = {
+        "purple": {"colour_1": "FFE2F2", "colour_2": "FFCCE8"},
+        "blue": {"colour_1": "E3FAFF", "colour_2": "BCE7F1"},
+        "green": {"colour_1": "F3F6F4", "colour_2": "D7EADE"},
+    }
+    directory = pathlib.Path(__file__).parent.resolve()
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(directory))
+    base = env.get_template("macbeth.json.j2")
+    for colour in configurations:
+        rendered = base.render(configurations[colour])
+        output = pathlib.Path("macbeth_" + colour + ".json")
+        with open(directory.parent / "themes" / output, "w+") as file:
+            json.dump(json.loads(rendered), file, indent=4)
+
+
+if __name__ == "__main__":
+    generate_jsons()
